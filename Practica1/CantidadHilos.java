0e5395aa0292cb9d8e0e1d65d01b618a3d79a48d
Isaac Robledo <isaacrobled@ciencias.unam.mx>
2025-08-28 01:15:26 -0600

    docs: add javadoc

--- Practica1/CantidadHilos.java
+++ Practica1/CantidadHilos.java
@@ -1,9 +1,10 @@
-
+/**
+ * Clase que muestra la cantidad de hilos disponibles en tiempo de ejecución.
+ */
 public class CantidadHilos {
 
   public static void main(String[] args) {
     int cores = Runtime.getRuntime().availableProcessors();
-    System.out.println("el numero de hilos maximos es: "+ cores);
+    System.out.println("El numero de hilos maximos es: " + cores);
   }
-  
 }
